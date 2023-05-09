07b88e39ff2c7eb31385f9547b1b8f59dd7b468a
kotarokomori <komopro.works@gmail.com>
2023-05-10 00:56:11 +0900

    不要な記述を削除しました

--- src/components/util/Button.tsx
+++ src/components/util/Button.tsx
@@ -7,7 +7,7 @@ export const Button: React.FC<Props> = (props) => {
   const { className, children } = props
 
   return (
-    <a href="#" role="button" className={`block rounded-button duration-200 ${className}う`}>
+    <a href="#" role="button" className={`block rounded-button duration-200 ${className}`}>
       {children}
     </a>
   )
